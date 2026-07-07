828c9e9430ebc701edf66b6c727a7bd69a1afc3f
Olin <olin@labyrinthai.co>
2026-07-07 14:16:55 -0400

    better comments

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## DelphiToLeRobot
 
-### Delphi v1
+### Delphi => LeRobotDataset
+Turns a dataset downloaded from Delphi (with given version) to a LeRobotDataset of desired format.
+
+### Compositor
+Composites a raw video with a robot overlay, downloaded from delphi.
+
+
--- composite.py
+++ composite.py
@@ -2,13 +2,16 @@ import subprocess
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python3 composite.py <raw_video> <robot_overlay>")
+        sys.exit(1)
     raw_video = sys.argv[1]
     overlay_video = sys.argv[2]
     output_path = "composite.mp4"
     subprocess.run([
         "ffmpeg",
-        "-i", str(raw_video),       # background (normal video)
-        "-i", str(overlay_video),   # foreground (arm on red background)
+        "-i", str(raw_video),       # background (raw video)
+        "-i", str(overlay_video),   # foreground (robot on red background)
         "-filter_complex",
         "[1:v]colorkey=0xFF0000:0.3:0.1[fg];[0:v][fg]overlay",
         "-c:v", "libx264",
