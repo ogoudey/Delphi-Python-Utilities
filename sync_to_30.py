02df901a31c204666af5ed4395c8dd34eab674c4
Olin Goudey <olin@labyrinthai.co>
2026-08-25 21:23:56 -0400

    2 new commands supported

--- sync_to_30.py
+++ sync_to_30.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 def sync_video_to_30_fps(video_path: Path, delete_original: bool = False):
-    output_path = video_path.parent / f"{video_path.name}.30fps.mp4"
+    output_path = video_path.with_suffix(".30fps.mp4")
     subprocess.run([
         "ffmpeg",
         "-i", video_path,
@@ -18,13 +18,31 @@ def sync_video_to_30_fps(video_path: Path, delete_original: bool = False):
     if delete_original:
         video_path.unlink()
 
-def main(dataset: Path):
+def main(dataset: Path, delete_original: bool = False):
+    cnt = 0
     for path in dataset.iterdir():
-        if path.is_file() and path.suffix == ".mp4":
-            sync_video_to_30_fps(path)
+        if path.is_file() and path.suffix == ".mp4" and not path.name.endswith(".30fps.mp4"):
+            cnt += 1
+            sync_video_to_30_fps(path, delete_original)
+    print(f"Synced {cnt} videos to 30 fps in {dataset}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python3 sync_to_30.py <dataset_path>")
-    
+        print("Usage: python3 sync_to_30.py <dataset_path> <bool: delete_original = false>")
+        sys.exit(1)
+
     root = Path(sys.argv[1])
+    if len(sys.argv) > 2:
+        main(root, delete_original=sys.argv[2].lower() == "true")
+    else:
+        main(root)
+
+"""
+Use in WF:
+
+```
+python3 sync_to_30.py tmp/data true
+```
+
+
+"""
